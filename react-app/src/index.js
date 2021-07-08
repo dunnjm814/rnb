3e89da49d3c9b20e11179ad2d5bb6643931bc871
Jason Dunn <dunnjm814@gmail.com>
2021-07-07 17:03:00 -0700

    installed appropriate sass version. Imported fonts and assigned color variables for use in main

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
-import './index.css';
+import './index.scss';
 import App from './App';
 import configureStore from './store';
 
